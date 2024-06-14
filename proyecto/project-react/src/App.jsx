89424b71301ea64d43a3c91a96449b65148ab67e
Camilo MIranda <camilodejesusmirandaribon@gmail.com>
2024-06-14 15:40:51 -0500

    Agregando rutas al proyecto

--- proyecto/project-react/src/App.jsx
+++ proyecto/project-react/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Card from "./components/CardComponent";
+import HomePage from "./components/Pages/HomePage";
+import NotFoundPage from "./components/Pages/NotFoundPage";
 
 function App() {
   const initialPage = () => {
@@ -17,7 +20,7 @@ function App() {
 
   useEffect(() => {
     fetchCharacters();
-  }, [pagina]);
+  }, [pagina, nameFilter, genderFilter, statusFilter, typeFilter]);
 
   const fetchCharacters = () => {
     const filterParams = new URLSearchParams({
@@ -58,87 +61,115 @@ function App() {
     fetchCharacters();
   };
 
-  return (
-    <div>
+  const SearchForm = () => (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleSearch();
+      }}
+    >
       <div>
-        <h1>Rick and Morty Characters</h1>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleSearch();
-          }}
-        >
-          <div>
-            <label>
-              Name:
-              <input
-                type="text"
-                value={nameFilter}
-                onChange={(e) => setNameFilter(e.target.value)}
-              />
-            </label>
-          </div>
-          <div>
-            <label>
-              Gender:
-              <select
-                value={genderFilter}
-                onChange={(e) => setGenderFilter(e.target.value)}
-              >
-                <option value="">Select</option>
-                <option value="female">Female</option>
-                <option value="male">Male</option>
-                <option value="genderless">Genderless</option>
-                <option value="unknown">Unknown</option>
-              </select>
-            </label>
-          </div>
-          <div>
-            <label>
-              Status:
-              <select
-                value={statusFilter}
-                onChange={(e) => setStatusFilter(e.target.value)}
-              >
-                <option value="">Select</option>
-                <option value="alive">Alive</option>
-                <option value="dead">Dead</option>
-                <option value="unknown">Unknown</option>
-              </select>
-            </label>
-          </div>
-          <div>
-            <label>
-              Type:
-              <input
-                type="text"
-                value={typeFilter}
-                onChange={(e) => setTypeFilter(e.target.value)}
-              />
-            </label>
-          </div>
-          <button type="submit">Search</button>
-        </form>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={nameFilter}
+            onChange={(e) => setNameFilter(e.target.value)}
+          />
+        </label>
       </div>
-      <div className="card-container">
-        {personajes.length !== 0 &&
-          personajes.map((personaje) => (
-            <Card
-              key={personaje.id}
-              title={personaje.name}
-              img={personaje.image}
-              genre={personaje.gender}
-              status={personaje.status}
-            />
-          ))}
-        <div>
-          <button onClick={handlePreviousPage} disabled={pagina === 1}>
-            Anterior
-          </button>
-          <button onClick={handleNextPage}>Siguiente</button>
-        </div>
+      <div>
+        <label>
+          Gender:
+          <select
+            value={genderFilter}
+            onChange={(e) => setGenderFilter(e.target.value)}
+          >
+            <option value="">Select</option>
+            <option value="female">Female</option>
+            <option value="male">Male</option>
+            <option value="genderless">Genderless</option>
+            <option value="unknown">Unknown</option>
+          </select>
+        </label>
+      </div>
+      <div>
+        <label>
+          Status:
+          <select
+            value={statusFilter}
+            onChange={(e) => setStatusFilter(e.target.value)}
+          >
+            <option value="">Select</option>
+            <option value="alive">Alive</option>
+            <option value="dead">Dead</option>
+            <option value="unknown">Unknown</option>
+          </select>
+        </label>
+      </div>
+      <div>
+        <label>
+          Type:
+          <input
+            type="text"
+            value={typeFilter}
+            onChange={(e) => setTypeFilter(e.target.value)}
+          />
+        </label>
+      </div>
+      <button type="submit">Search</button>
+    </form>
+  );
+
+  const CharacterList = () => (
+    <div className="card-container">
+      {personajes.length !== 0 &&
+        personajes.map((personaje) => (
+          <Card
+            key={personaje.id}
+            title={personaje.name}
+            img={personaje.image}
+            genre={personaje.gender}
+            status={personaje.status}
+          />
+        ))}
+      <div>
+        <button onClick={handlePreviousPage} disabled={pagina === 1}>
+          Anterior
+        </button>
+        <button onClick={handleNextPage}>Siguiente</button>
       </div>
     </div>
+  );
+
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/notfound">Not Found Page</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <h1>Rick and Morty Characters</h1>
+              <SearchForm />
+              <CharacterList />
+            </div>
+          }
+        />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ proyecto/project-react/src/components/Pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// TODO: agrega estilos un diseño personalizado a la página
+function HomePage(props) {
+  return (
+    <div>
+      <h1> Bienvenido a la pagina de rick and morty</h1>
+    </div>
+  );
+}
+export default HomePage;
